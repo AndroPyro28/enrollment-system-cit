10eae53ba99b35967d01080975b776e835ecce73
AndroPyro28 <menandroeugenio1028@gmail.com>
2024-09-19 10:59:15 -0400

    learners information not done yet

--- app/(main)/[role]/(templates)/learners-information/components/LearnersInformationForm.tsx
+++ app/(main)/[role]/(templates)/learners-information/components/LearnersInformationForm.tsx
@@ -13,6 +13,8 @@ import {
   CreateRegistrationFormSchema,
   CreateRegistrationFormT,
 } from "@/schema/registrations";
+import { Separator } from "@/components/ui/separator"
+import { Textarea } from "@/components/ui/textarea"
 import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
@@ -106,7 +108,7 @@ export const LearnersInformationForm = () => {
                   )}
                   />
 
-<FormField
+                <FormField
                   control={form.control}
                   name="is_returnee"
                   render={({ field }) => (
@@ -139,97 +141,252 @@ export const LearnersInformationForm = () => {
                   </div>
                 </div>
             </div>
-
+            <Separator />
           <div className="flex flex-col space-y-5">
-            <h3 className="uppercase">Instruction</h3>
+            <div className="text-center">
+                <h1 className="uppercase font-semibold">Learners Information</h1>
+            </div>
+            <div>
+              <h3 className="uppercase">Instruction</h3>
+            </div>
             <p className="text-sm">
                 Print legibly all information required in CAPITAL LETTERS. Submit accomplished form in the Person-in-Charge / Registrar / Class Adviser. Use black or Blue pen only.
             </p>
 
-            <FormField
-              control={form.control}
-              name="preferred_learning_modalities"
-              render={() => {
-                const items = [
-                  {
-                    id: "Modular_Print",
-                    label: "Modular (Print)",
-                  },
-                  {
-                    id: "online",
-                    label: "Online",
-                  },
-                  {
-                    id: "radio_based_instruction",
-                    label: "Radio Based Instruction",
-                  },
-                  {
-                    id: "blended",
-                    label: "Blended",
-                  },
-                  {
-                    id: "modular_online",
-                    label: "Modular (Online)",
-                  },
-                  {
-                    id: "education_television",
-                    label: "Education Television",
-                  },
-                  {
-                    id: "home_schooling",
-                    label: "Homeschooling",
-                  },
-                  
-                ] as const;
-
-                return (
-                  <FormItem className="">
-                    <div className="mb-4">
-                      <FormLabel className="text-sm">Choose all that applies</FormLabel>
+            <div className="flex flex-col">
+                <div className="flex justify-evenly space-x-1">
+                  <div className="flex justify-between items-center space-x-3">
+                  <p className="text-sm">PSA Birthday Certificate No. (if available upon registration)</p>
+                   <FormField
+                    control={form.control}
+                    name={"psa_birth_cert_no"}
+                    render={({ field }) => (
+                      <FormItem className="">
+                        <FormControl>
+                          <Input
+                            id="upload"
+                              />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                    />
                     </div>
-                    <div className="grid grid-cols-4 grid-rows-2 space-y-3 items-center">
-                    {items.map((item) => (
-                      <FormField
-                        key={item.id}
+                    <div className="flex justify-between items-center space-x-3">
+                      <p className="text-sm">Learner Reference No.</p>
+                   <FormField
+                    control={form.control}
+                    name={"lrn_no"}
+                    render={({ field }) => (
+                      <FormItem className="">
+                        <FormControl>
+                          <Input
+                            id="upload"
+                            className=""
+                            />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                    />
+                    </div>
+                </div>
+                <div className="grid grid-cols-3 grid-rows-1 space-x-10 mt-5">
+                    <div className="flex flex-col space-y-3">
+                    <FormField
+                    control={form.control}
+                    name={"lrn_no"}
+                    render={({ field }) => (
+                      <FormItem className="">
+                        <FormLabel>
+                          Last Name
+                        </FormLabel>
+                        <FormControl>
+                          <Input
+                            id="upload"
+                            placeholder="Last Name"
+                            className=""
+                            />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                    />
+                    <FormField
+                    control={form.control}
+                    name={"lrn_no"}
+                    render={({ field }) => (
+                      <FormItem className="">
+                        <FormLabel>
+                          First Name
+                        </FormLabel>
+                        <FormControl>
+                          <Input
+                            id="upload"
+                            className=""
+                            placeholder="First Name"
+                            />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                    />
+                    <FormField
+                    control={form.control}
+                    name={"lrn_no"}
+                    render={({ field }) => (
+                      <FormItem className="">
+                        <FormLabel>
+                          Middle Name
+                        </FormLabel>
+                        <FormControl>
+                          <Input
+                            id="upload"
+                            className=""
+                            placeholder="Middle Name"
+                            />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                    />
+                    <FormField
+                    control={form.control}
+                    name={"lrn_no"}
+                    render={({ field }) => (
+                      <FormItem className="">
+                        <FormLabel>
+                          Extension Name e.g JR, III (if applicable)
+                        </FormLabel>
+                        <FormControl>
+                          <Input
+                            id="upload"
+                            className=""
+                            placeholder="Extension Name"
+                            />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                    />
+                    </div>
+                    <div className="col-span-2 flex flex-col ">
+                      <div className="flex space-x-5">
+                        <div className="flex flex-col">
+                        <FormField
                         control={form.control}
-                        name="preferred_learning_modalities"
-                        render={({ field }) => {
-                          return (
-                            <FormItem
-                              key={item.id}
-                              className="flex flex-row items-start space-x-3 space-y-0"
-                            >
-                              <FormControl>
-                                <Checkbox
-                                  checked={field.value?.includes(item.id)}
-                                  onCheckedChange={(checked) => {
-                                    return checked
-                                      ? field.onChange([
-                                          ...field.value ?? [],
-                                          item.id,
-                                        ])
-                                      : field.onChange(
-                                          field.value?.filter(
-                                            (value) => value !== item.id
-                                          )
-                                        );
-                                  }}
+                        name={"lrn_no"}
+                        render={({ field }) => (
+                          <FormItem className="flex flex-col">
+                            <FormLabel>
+                              Birthday (mm/dd/yyyy)
+                            </FormLabel>
+                            <FormControl>
+                              <Input
+                                id="upload"
+                                className=""
                                 />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                        />
+                        <div className="flex space-x-5">
+                        <FormField
+                          control={form.control}
+                          name="gender"
+                          render={({ field }) => (
+                            <FormItem className="space-y-3 flex ">
+                              <FormLabel className="mt-3">Semester</FormLabel>
+                              <FormControl>
+                                <RadioGroup
+                                  onValueChange={field.onChange}
+                                  defaultValue={field.value}
+                                  className="flex space-x-3 items-center ml-5 flex-col space-y-3"
+                                >
+                                  <FormItem className="flex items-center  space-x-3 space-y-0">
+                                    <FormControl>
+                                      <RadioGroupItem value="male" />
+                                    </FormControl>
+                                    <FormLabel className="font-normal">male</FormLabel>
+                                  </FormItem>
+                                  <FormItem className="flex items-center space-x-3 space-y-0">
+                                    <FormControl>
+                                      <RadioGroupItem value="female" />
+                                    </FormControl>
+                                    <FormLabel className="font-normal">female</FormLabel>
+                                  </FormItem>
+                                </RadioGroup>
                               </FormControl>
-                              <FormLabel className="font-normal">
-                                {item.label}
-                              </FormLabel>
+                              <FormMessage />
                             </FormItem>
-                          );
-                        }}
-                      />
-                    ))}
+                          )}
+                        />
+                        <FormField
+                        control={form.control}
+                        name={"age"}
+                        render={({ field }) => (
+                          <FormItem className="mt-2">
+                            <FormLabel>Age</FormLabel>
+                            <FormControl>
+                              <Input
+                                id="upload"
+                                placeholder="Age"
+                                className=""
+                                />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                        />
+                        </div>
+                        </div>
+                        <div className="flex flex-col w-full space-y-3">
+                        <FormField
+                        control={form.control}
+                        name={"place_of_birth"}
+                        render={({ field }) => (
+                          <FormItem className="w-full">
+                            <FormControl>
+                              <Textarea
+                                id="upload"
+                                placeholder="Place of birth"
+                                className="w-full"
+                                />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                        />
+
+                      <FormField
+                        control={form.control}
+                        name={"mother_tongue"}
+                        render={({ field }) => (
+                          <FormItem className="w-full">
+                            <FormControl>
+                              <Textarea
+                                id="upload"
+                                placeholder="Mother tounge"
+                                className="w-full"
+                                />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                        />
+                        </div>
+                      </div>
+
+                      <div>
+                        <p>Belonging to any indigenous People (IP) Community/Indigenous Culture community</p>
+                      </div>
+
+
                     </div>
-                    <FormMessage />
-                  </FormItem>
-                );
-              }}
-            />
+                </div>
+            </div>
+                    {/* end */}
           </div>
           <Button className="w-fit self-end">Next</Button>
         </form>
